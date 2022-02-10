b632958c47cbf2a0876852b907415321664bb1a9
ocaisa <a.ocais@fz-juelich.de>
2022-02-10 09:48:38 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="amdahl",
-    version="1.0",
+    version="0.1",
     author="Alan O'Cais",
     author_email="alan.ocais@cecam.org",
     python_requires=">=3",
